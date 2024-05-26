2e0c62738973cb7f4251be55205794495c043d02
erikkaugusto <erik.ahlert@universo.univates.br>
2024-05-26 20:44:25 -0300

    aplicando audio e estilos

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alterar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3'); // pegar o audio
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+})
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active');
+    })
+
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
 
@@ -29,14 +45,17 @@ function alterarContexto(contexto) {
 
 focoBt.addEventListener('click', () => { // arrow function - quando o botão for clicado chama a função
     alterarContexto('foco');
+    focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
 })
 
 
